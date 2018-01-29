010cf00ec0963ccca9c1f02c207910ef99cd23d9
Nathan Lee <nathanlee@nathans-mbp.bc.edu>
2018-01-29 16:08:06 -0500

    Finished up to 2.17

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -7,31 +7,93 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var soundSwitch: UISwitch!
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var awesomeImage: UIImageView!
+    var awesomePlayer = AVAudioPlayer()
+    var index = -1
+    var imageNumber = -1
+    var soundNumber = -1
+    let numberOfImages = 5
+    let numberOfSounds = 4
+    
+    //MARK: - Functions
     
     // Code below executes when the app's view first loads
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    @IBAction func showMessagePressed(_ sender: UIButton) {
-        if messageLabel.text == "You are Awesome!"{
-            messageLabel.text = "You are Great!"
-            messageLabel.textColor = UIColor.blue
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer){
+        //can we load in the file soundName?
+        if let sound = NSDataAsset(name: soundName){
+            //check if sound.data is a sound file
+            do{
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch{
+                //if sound.data is not a valid audio file
+                print("ERROR: data in \(soundName) couldn't be played as a sound")
+            }
+            
         }
-        else if messageLabel.text == "You are Great!"{
-            messageLabel.text = "You are Amazing!"
-            messageLabel.textColor = UIColor.red
-        }
+            //if NSDataAsset can't read it, tell the user/report the error
         else{
-            messageLabel.text = "You are Awesome!"
-            messageLabel.textColor = UIColor.green
+            print("ERROR: file \(soundName) didn't load")
         }
-
-       
+    }
+    
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int{
+        var newIndex = -1
+        repeat{
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        } while lastNumber == newIndex
+        return newIndex
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction func soundSwitchPressed(_ sender: UISwitch) {
+        if(soundNumber > -1 && !soundSwitch.isOn){
+            awesomePlayer.stop()
+        }
+    }
+    
+    //lots of things that happen when you press the button
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        
+        let messages = ["You are Fantastic!",
+                        "You are Great!",
+                        "You are Amazing!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You Brighten My Day!",
+                        "You are Da Bomb!",
+                        "I can't wait to use your app!",
+                        "Fabulous? That's You!"]
+        
+        //show a random message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
+        messageLabel.text = messages[index]
+        
+        //show a random image
+        imageNumber = nonRepeatingRandom(lastNumber: imageNumber, maxValue: numberOfImages)
+        awesomeImage.image = UIImage(named: "\(imageNumber)")
+        
+        //play a random sound
+        if soundSwitch.isOn == true{
+            soundNumber = nonRepeatingRandom(lastNumber: soundNumber, maxValue: numberOfSounds)
+            let soundName = "sound\(soundNumber)"
+            playSound(soundName: soundName, audioPlayer: &awesomePlayer)
+            
+        }
+        
     }
     
     
